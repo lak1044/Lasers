e8d66b8717a29bfa855c5617c386c5b547218e4d
mxb1295 <mxb1295@cs.rit.edu>
2016-04-11 13:49:26 -0400

    Initial commit to test merge

---
+++ src/LasersPTUI.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Mooky on 4/11/2016.
+ */
+public class LasersPTUI {
+    public static void main(String[] args) {
+        System.out.println("My name is Mooky Boeck-Chenevier");
+        System.out.println("My name is Lucas Kretvix");
+        System.out.println("Our project account is p142-05n");
+    }
+}
