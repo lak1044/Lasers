a539412ee54c2c6e8b7d9430c999009c60caa162
lak1044 <lak1044@rit.edu>
2016-04-14 14:34:28 -0400

    Changed last for loop in toString for submission

--- src/LaserModel.java
+++ src/LaserModel.java
@@ -307,10 +307,14 @@ public class LaserModel {
         for (int i = 0; i < rows; i++) {
             result += i % 10 + "|";
             for (int j = 0; j < cols; j++) {
-                //if (j == cols - 1) {
-                    //result += lGrid[i][j];
-                  //  continue;
-                //}
+                if (j == cols - 1 && i == rows - 1){
+                    result += lGrid[i][j];
+                    continue;
+                }
+                else if (j == cols - 1) {
+                    result += lGrid[i][j] + "\n";
+                    continue;
+                }
                 result += lGrid[i][j] + " ";
             }
         }
