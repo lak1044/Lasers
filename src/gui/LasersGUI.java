0265390917114a0d351a4b4fa475dbc6b4c66193
mxb1295 <mxb1295@cs.rit.edu>
2016-05-06 11:45:32 -0400

    Initial commit for GUI. Layout and constructor work. No functionality or update.

--- src/gui/LasersGUI.java
+++ src/gui/LasersGUI.java
@@ -1,8 +1,10 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -28,6 +30,13 @@ public class LasersGUI extends Application implements Observer {
 
     /** this can be removed - it is used to demonstrates the button toggle */
     private static boolean status = true;
+
+    /** state of borderPane for GUI */
+    private BorderPane borderPane;
+
+    /** state of buttons for update() */
+    private GridPane buttonGrid;
+
 
     @Override
     public void init() throws Exception {
@@ -91,13 +100,161 @@ public class LasersGUI extends Application implements Observer {
         stage.setScene(scene);
     }
 
+
+    private GridPane constructSafeGrid(){
+        buttonGrid = new GridPane();
+        for (int i=0; i<model.rows; i++) {
+            for (int j = 0; j < model.cols; j++) {
+
+                char toCheck = model.GetVal(i,j);
+
+
+                //construct emptyButton
+                if (toCheck == '.'){
+                    Button newButton = new Button();
+                    Image whiteImg = new Image(getClass().getResourceAsStream("resources/white.png"));
+                    ImageView whiteIcon = new ImageView(whiteImg);
+                    newButton.setGraphic(whiteIcon);
+                    setButtonBackground(newButton, "white.png");
+                    int irow= i;
+                    int jcol= j;
+                    newButton.setOnAction(event -> model.Add(irow,jcol));
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Laser
+                else if (toCheck == 'L'){
+                    Button newButton = new Button();
+                    Image laserImg = new Image(getClass().getResourceAsStream("resources/laser.png"));
+                    ImageView laserIcon = new ImageView(laserImg);
+                    newButton.setGraphic(laserIcon);
+                    setButtonBackground(newButton, "yellow.png");
+                    int irow= i;
+                    int jcol= j;
+                    newButton.setOnAction(event -> model.Remove(irow,jcol));
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Beam
+                else if (toCheck == '*'){
+                    Button newButton = new Button();
+                    Image beamImg = new Image(getClass().getResourceAsStream("resources/beam.png"));
+                    ImageView beamIcon = new ImageView(beamImg);
+                    newButton.setGraphic(beamIcon);
+                    setButtonBackground(newButton, "yellow.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct AnyPillar
+                else if (toCheck == 'X'){
+                    Button newButton = new Button();
+                    Image pillarXImg = new Image(getClass().getResourceAsStream("resources/pillarX.png"));
+                    ImageView pillarXIcon = new ImageView(pillarXImg);
+                    newButton.setGraphic(pillarXIcon);
+                    setButtonBackground(newButton, "white.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Pillar0
+                else if (toCheck == '0'){
+                    Button newButton = new Button();
+                    Image pillar0Img = new Image(getClass().getResourceAsStream("resources/pillar0.png"));
+                    ImageView pillar0Icon = new ImageView(pillar0Img);
+                    newButton.setGraphic(pillar0Icon);
+                    setButtonBackground(newButton, "white.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Pillar1
+                else if (toCheck == '1'){
+                    Button newButton = new Button();
+                    Image pillar1Img = new Image(getClass().getResourceAsStream("resources/pillar1.png"));
+                    ImageView pillar1Icon = new ImageView(pillar1Img);
+                    newButton.setGraphic(pillar1Icon);
+                    setButtonBackground(newButton, "white.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Pillar2
+                else if (toCheck == '2'){
+                    Button newButton = new Button();
+                    Image pillar2Img = new Image(getClass().getResourceAsStream("resources/pillar2.png"));
+                    ImageView pillar2Icon = new ImageView(pillar2Img);
+                    newButton.setGraphic(pillar2Icon);
+                    setButtonBackground(newButton, "white.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Pillar3
+                else if (toCheck == '3'){
+                    Button newButton = new Button();
+                    Image pillar3Img = new Image(getClass().getResourceAsStream("resources/pillar3.png"));
+                    ImageView pillar3Icon = new ImageView(pillar3Img);
+                    newButton.setGraphic(pillar3Icon);
+                    setButtonBackground(newButton, "white.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                //construct Pillar4
+                else if (toCheck == '4'){
+                    Button newButton = new Button();
+                    Image pillar4Img = new Image(getClass().getResourceAsStream("resources/pillar4.png"));
+                    ImageView pillar4Icon = new ImageView(pillar4Img);
+                    newButton.setGraphic(pillar4Icon);
+                    setButtonBackground(newButton, "white.png");
+                    buttonGrid.add(newButton, j, i);
+                }
+                /*
+                Button newButton = new Button();
+                Image whiteImg = new Image(getClass().getResourceAsStream("resources/white.png"));
+                ImageView whiteIcon = new ImageView(whiteImg);
+                newButton.setGraphic(whiteIcon);
+                setButtonBackground(newButton, "yellow.png");
+                buttonGrid.add(newButton, j, i);
+                */
+
+            }
+        }
+        return buttonGrid;
+    }
+
+
+    private HBox constructCommandButtons (){
+        HBox commandBox = new HBox();
+        Button checkButton = new Button("Check");
+        checkButton.setOnAction(event -> model.Verify());
+        Button hintButton = new Button("Hint");
+        //hintButton.setOnAction(event -> model.FINDMETHOD);
+        Button solveButton = new Button("Solve");
+        //solveButton.setOnAction(event -> FIND METHOD);
+        Button restartButton = new Button("Restart");
+        //restartButton.setOnAction(event -> model.restart);
+        Button loadButton = new Button("Load");
+        //loadButton.setOnAction(event -> FIND METHOD);
+
+        commandBox.getChildren().addAll(checkButton,hintButton,solveButton,restartButton,loadButton);
+
+        return commandBox;
+
+    }
+
+
+
+
     /**
      * The
      * @param stage the stage to add UI components into
      */
     private void init(Stage stage) {
         // TODO
-        buttonDemo(stage);  // this can be removed/altered
+
+        borderPane = new BorderPane();
+        Label statusLabel = new Label(model.fileName + " loaded");
+        statusLabel.setAlignment(Pos.CENTER);
+        borderPane.setTop(statusLabel);
+        borderPane.setCenter(constructSafeGrid());
+        borderPane.setBottom(constructCommandButtons());
+
+
+
+        Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
+
+        /*this.model = new LasersModel();
+        this.model.addObserver(this);*/
+        //buttonDemo(stage);  // this can be removed/altered
     }
 
     @Override
